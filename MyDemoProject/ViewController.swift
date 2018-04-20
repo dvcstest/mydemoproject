c6b2ef47e8fefa84192c079a420fc841516205b7
Francis Chary <francis@fchary-mbp-15.local>
2018-04-20 13:08:32 -0700

    Initial Commit

---
+++ MyDemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDemoProject
+//
+//  Created by Francis Chary on 2018-04-20.
+//  Copyright © 2018 Francis Chary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
